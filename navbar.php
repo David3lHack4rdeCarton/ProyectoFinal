04b91fd4f78b92bb76c2b9a139522e688ff77090
David3lHack4rdeCarton <santospacheco.angeldavid@utacapulco.edu.mx>
2023-03-30 18:46:46 -0600

    cambio

--- navbar.php
+++ navbar.php
@@ -9,43 +9,43 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/CrudMongo/Vistaprincipal.php">MongoBlog</a>
+  <a class="navbar-brand" href="/Vistaprincipal.php">MongoBlog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/CrudMongo/Vistaprincipal.php">Inicio <span class="sr-only">(actual)</span></a>
+        <a class="nav-link" href="/Vistaprincipal.php">Inicio <span class="sr-only">(actual)</span></a>
       </li>
       
       <li class="nav-item ">
-        <a class="nav-link" href="/CrudMongo/vistablogs/vistauserblog.php">BLOGS PARA TODOS </a>
+        <a class="nav-link" href="/vistablogs/vistauserblog.php">BLOGS PARA TODOS </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="/CrudMongo/Categoria/Consultacat.php">Categoría</a>
+        <a class="nav-link" href="/Categoria/Consultacat.php">Categoría</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/CrudMongo/Blog/blog.php">Blog</a>
+        <a class="nav-link" href="/Blog/blog.php">Blog</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/CrudMongo/Imagen/selectimg.php">Imagen</a>
+        <a class="nav-link" href="/Imagen/selectimg.php">Imagen</a>
       </li>
       <li class="nav-item">
-          <a class="nav-link" href="/CrudMongo/Entrada/Entrada.php">Entrada</a>
+          <a class="nav-link" href="/Entrada/Entrada.php">Entrada</a>
       </li>
       <li class="nav-item">
-          <a class="nav-link" href="/CrudMongo/Comentario/Comentarios.php">Comentario</a>
+          <a class="nav-link" href="/Comentario/Comentarios.php">Comentario</a>
       </li>
       <li class="nav-item">
-          <a class="nav-link" href="/CrudMongo/Usuarios/Consultuser.php">Usuarios</a>
+          <a class="nav-link" href="/Usuarios/Consultuser.php">Usuarios</a>
       </li>
 
     </ul>
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="/CrudMongo/login/Destruirsesion.php">Cerrar sesión</a>
+        <a class="nav-link" href="/login/Destruirsesion.php">Cerrar sesión</a>
       </li>
     </ul>
   </div>
